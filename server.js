c719fb93a595fc50e3ed77168fce3e4838c4e99f
acoustic0322 <sounds_on_street@yahoo.co.jp>
2022-05-07 17:12:28 +0900

    momonga1banchi様の以下のソースコードのクローン https://github.com/crowdworkssample001/mysqlsample001/

---
+++ server.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const express = require("express");
+const { Server } = require("ws");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = "/index.html";
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected:");
+  ws.onmessage = (event) => {
+    console.log("event.data: " + event.data);
+    wss.clients.forEach((client) => {
+      client.send(event.data);
+    });
+  };
+  ws.on("close", () => console.log("Client disconnected"));
+});
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send("");
+  });
+}, 10000);
